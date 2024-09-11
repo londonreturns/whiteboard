3770353d536b1e2ac96c1d943d828b3416431bae
londonreturns <subedioscar.47@gmail.com>
2024-09-11 12:44:21 +0545

    marker colors added

--- eo.py
+++ eo.py
@@ -8,9 +8,11 @@ import time
 def calculate_distance(point1, point2):
     return math.sqrt((point2[0] - point1[0]) ** 2 + (point1[1] - point2[1]) ** 2)
 
-def extract(cv2):
+def extract():
     gray_canvas = cv2.cvtColor(marker_canvas, cv2.COLOR_BGR2GRAY)
-    text = reader.readtext(gray_canvas, detail=0, batch_size=5)
+    _, threshold_canvas = cv2.threshold(gray_canvas, 0, 255, cv2.THRESH_BINARY)
+    text = reader.readtext(threshold_canvas, detail=0, batch_size=5)
+    cv2.imwrite("output.jpg", threshold_canvas)
     print("Recognized Text:", " ".join(text))
 
 mp_hands = mp.solutions.hands
@@ -22,7 +24,6 @@ cap = cv2.VideoCapture(0)
 
 marker_canvas = None
 button_canvas = None
-
 prev_x, prev_y = None, None
 
 marker_on = True
@@ -33,10 +34,11 @@ two_hands_detected = False
 hover_start_time = None
 last_extract_time = 0
 last_clear_time = 0
+marker_thickness = 3
 
 draw_buttons = True
 
-colors = {
+button_colors = {
     "yellow": (0, 255, 255),
     "green": (0, 255, 0)
 }
@@ -45,6 +47,16 @@ buttons = [
     ["Clear Screen", "yellow"],
     ["Text Extraction", "green"],
 ]
+
+marker_colors = {
+    "white": (255, 255, 255),
+    "orange": (39, 132, 255),
+    "purple": (215, 71, 143)
+}
+
+marker_color = marker_colors["white"]
+
+marker_boxes = []
 
 clear_button_start = (92, 10)
 clear_button_end = (274, 100)
@@ -75,11 +87,24 @@ while True:
         gap, width = 92, 182
         axis_x, axis_y = gap, 0
         for i, button in enumerate(buttons):
-            cv2.rectangle(button_canvas, (axis_x, 10), (axis_x + width, 100), colors[button[1]], 5)
-            cv2.putText(button_canvas, button[0], (axis_x + 18, 60), cv2.FONT_HERSHEY_SIMPLEX, 0.6, colors[button[1]], 2)
+            cv2.rectangle(button_canvas, (axis_x, 10), (axis_x + width, 100), button_colors[button[1]], 5)
+            cv2.putText(button_canvas, button[0], (axis_x + 18, 60), cv2.FONT_HERSHEY_SIMPLEX,
+                        0.6, button_colors[button[1]], 2)
             axis_x += gap + width
+
+        gap, width = 115, 50
+        axis_x, axis_y = 10, gap
+        for name, color in marker_colors.items():
+            box_start = (axis_x, axis_y)
+            box_end = (axis_x + width, axis_y + 50)
+            cv2.rectangle(button_canvas, box_start, box_end, marker_colors[name], -1)
+            marker_boxes.append((box_start, box_end, marker_colors[name]))
+            axis_y += 50 + 50
+
         draw_buttons = False
 
+    if result is None:
+        continue
     if result.multi_hand_landmarks:
         hand_count = len(result.multi_hand_landmarks)
 
@@ -94,14 +119,6 @@ while True:
                 print("marker on after 5 seconds")
             two_hands_detected = False
             two_hands_start_time = None
-        elif hand_count == 2:
-            marker_on = False
-            prev_x, prev_y = None, None
-            if not two_hands_detected:
-                two_hands_detected = True
-                two_hands_start_time = current_time
-                marker_canvas = np.zeros_like(frame)
-                print("two hands detected - canvas cleared")
         else:
             two_hands_detected = False
             one_hand_start_time = None
@@ -122,13 +139,18 @@ while True:
 
                 distance = calculate_distance(index_finger_pos, thumb_pos)
 
+                for box_start, box_end, color in marker_boxes:
+                    if box_start[0] < index_finger_pos[0] < box_end[0] and \
+                       box_start[1] < index_finger_pos[1] < box_end[1]:
+                        marker_color = color
+
                 if extract_button_start[0] < index_finger_pos[0] < extract_button_end[0] and \
                    extract_button_start[1] < index_finger_pos[1] < extract_button_end[1]:
                     if hover_start_time is None:
                         hover_start_time = current_time
                     elif current_time - hover_start_time >= 1:
                         if current_time - last_extract_time >= extract_buffer_time:
-                            extract(cv2)
+                            extract()
                             last_extract_time = current_time
                             hover_start_time = None
                 elif clear_button_start[0] < index_finger_pos[0] < clear_button_end[0] and \
@@ -146,7 +168,7 @@ while True:
 
                 if distance > 38:
                     if prev_x is not None and prev_y is not None:
-                        cv2.line(marker_canvas, (prev_x, prev_y), index_finger_pos, (255, 255, 255), 3)
+                        cv2.line(marker_canvas, (prev_x, prev_y), index_finger_pos, marker_color, marker_thickness)
                     prev_x, prev_y = index_finger_pos
                 else:
                     prev_x, prev_y = None, None
@@ -172,7 +194,7 @@ while True:
     elif key == ord(' '):
         marker_canvas = None
     elif key == ord('r'):
-        extract(cv2)
+        extract()
 
 cap.release()
 cv2.destroyAllWindows()
